67e255636e8a32d52578ac8da103e238270e50df
Matthew Slone <Matthew.Slone@outlook.com>
2018-02-05 11:13:17 -0500

    Display Main Menu from the DisplayMainMenu(); and call it from the Run();
    Also, added a text file of psuedocode listed in the GameSpaceRunner.Run();

--- TeamExercise4/GameSpaceRunner.cs
+++ TeamExercise4/GameSpaceRunner.cs
@@ -19,40 +19,7 @@ namespace TeamExercise4
             //Status Banner initialization
             //amoutnt $, State of cargo, Where we are
             SpaceBanner();
-            Console.WriteLine("Main Menu");
-            Console.WriteLine(" 1) Buy");
-            Console.WriteLine(" 2) Sell");
-            Console.WriteLine(" 3) Travel");
-            Console.WriteLine(" 4) Exit");
-            Console.Write("Please select an option:");
-            //present menu
-            //menu items
-            //Buy, Sell, Travel
-
-            //Buy Menu
-            //present list of items to select from
-            //water, dilithum crystal
-            //water is stored ship
-            //reduce $
-            //display cargo inventory and $ balance
-            //present main menu
-            //Sell Menu
-            //present list of items to select from
-            //water, dilithum crystal
-            //cargo updated and displayed in banner
-            //increase $
-            //display cargo inventory and $ balance
-            //present main menu
-            //Travel Menu
-            //present list of places to go
-            //AC, EARTH
-            //select speed (what cruise speed would you like to set in WF)
-            //www.calormen.com/star_trek/warpcalc/  v(W) = W(10/3) ) + (10 − W)(-11/3)
-            //CHANGE ship location
-            //update location in banner
-            //display message you traveled <> ly in <> (in time took)
-
-
+            DispayMainMenu();
         }
         public void SpaceBanner()
         {
@@ -60,7 +27,15 @@ namespace TeamExercise4
         }
         void DispayMainMenu()
         {
-            throw new NotImplementedException(); 
+            //present menu
+            //menu items
+            //Buy, Sell, Travel
+            Console.WriteLine("Main Menu");
+            Console.WriteLine(" 1) Buy");
+            Console.WriteLine(" 2) Sell");
+            Console.WriteLine(" 3) Travel");
+            Console.WriteLine(" 4) Exit");
+            Console.Write("Please select an option:");
         }
         void ElicitUserInput()
         {
